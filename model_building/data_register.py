7bbc7bb5d2ec112652e45384e1d0b4d336e8e8c4
vsakar <vsakar@gmail.com>
2025-12-07 10:28:08 +0000

    adding data folder for Hugging face upload

--- model_building/data_register.py
+++ model_building/data_register.py
@@ -19,7 +19,7 @@ except RepositoryNotFoundError:
     print(f"Space '{repo_id}' created.")
 
 api.upload_folder(
-    folder_path="/content/tourism_project/data",
+    folder_path="data",
     repo_id=repo_id,
     repo_type=repo_type,
 )
